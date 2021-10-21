df2ec86d3b5290cdcd53dad8bfe45a436d2b4f6a
Tieme van Veen <tiemevanveen@hotmail.com>
2021-10-21 13:42:08 +0200

    Added scheme management metadata properties to TargetScheme

--- Sources/ProjectSpec/TargetScheme.swift
+++ Sources/ProjectSpec/TargetScheme.swift
@@ -7,6 +7,7 @@ public struct TargetScheme: Equatable {
     public static let disableMainThreadCheckerDefault = false
     public static let stopOnEveryMainThreadCheckerIssueDefault = false
     public static let buildImplicitDependenciesDefault = true
+    public static let sharedDefault = true
 
     public var testTargets: [Scheme.Test.TestTarget]
     public var configVariants: [String]
@@ -21,6 +22,9 @@ public struct TargetScheme: Equatable {
     public var environmentVariables: [XCScheme.EnvironmentVariable]
     public var preActions: [Scheme.ExecutionAction]
     public var postActions: [Scheme.ExecutionAction]
+    public var shared: Bool
+    public var orderHint: Int?
+    public var isShown: Bool?
 
     public init(
         testTargets: [Scheme.Test.TestTarget] = [],
@@ -35,7 +39,10 @@ public struct TargetScheme: Equatable {
         commandLineArguments: [String: Bool] = [:],
         environmentVariables: [XCScheme.EnvironmentVariable] = [],
         preActions: [Scheme.ExecutionAction] = [],
-        postActions: [Scheme.ExecutionAction] = []
+        postActions: [Scheme.ExecutionAction] = [],
+        shared: Bool = sharedDefault,
+        orderHint: Int? = nil,
+        isShown: Bool? = nil
     ) {
         self.testTargets = testTargets
         self.configVariants = configVariants
@@ -45,11 +52,22 @@ public struct TargetScheme: Equatable {
         self.region = region
         self.disableMainThreadChecker = disableMainThreadChecker
         self.stopOnEveryMainThreadCheckerIssue = stopOnEveryMainThreadCheckerIssue
+        self.isShown = isShown
         self.buildImplicitDependencies = buildImplicitDependencies
         self.commandLineArguments = commandLineArguments
         self.environmentVariables = environmentVariables
         self.preActions = preActions
         self.postActions = postActions
+        self.shared = shared
+        self.orderHint = orderHint
+        self.postActions = postActions
+    }
+
+    public var management: Scheme.Management? {
+        if shared != TargetScheme.sharedDefault || orderHint != nil || isShown != nil {
+            return Scheme.Management(shared: shared,orderHint: orderHint,isShown: isShown)
+        }
+        return nil
     }
 }
 
@@ -81,6 +99,9 @@ extension TargetScheme: JSONObjectConvertible {
         environmentVariables = try XCScheme.EnvironmentVariable.parseAll(jsonDictionary: jsonDictionary)
         preActions = jsonDictionary.json(atKeyPath: "preActions") ?? []
         postActions = jsonDictionary.json(atKeyPath: "postActions") ?? []
+        shared = jsonDictionary.json(atKeyPath: "shared") ?? TargetScheme.sharedDefault
+        orderHint = jsonDictionary.json(atKeyPath: "orderHint")
+        isShown = jsonDictionary.json(atKeyPath: "isShown")
     }
 }
 
@@ -121,6 +142,18 @@ extension TargetScheme: JSONEncodable {
 
         if let region = region {
             dict["region"] = region
+        }
+
+        if shared != TargetScheme.sharedDefault {
+            dict["shared"] = shared
+        }
+
+        if let orderHint = orderHint {
+            dict["orderHint"] = orderHint
+        }
+
+        if let isShown = isShown {
+            dict["isShown"] = isShown
         }
 
         return dict
